43e9c561b4eb3a8eaef8618b80dab466819a8e01
Adam Tse <mr.adam.tse@gmail.com>
2016-12-04 03:54:35 -0500

    on create redirect to via router

--- blog/src/app/blog/post.service.ts
+++ blog/src/app/blog/post.service.ts
@@ -6,6 +6,8 @@ import { Post } from './post';
 @Injectable()
 export class PostService {
 
+  serverUrl = 'http://0.0.0.0:3000/api';
+
   constructor(private http: Http) { }
 
   headers = new Headers({
@@ -13,15 +15,21 @@ export class PostService {
   });
   // get all posts from api server
   getPosts(): Observable<Post[]>{
-    let url = "http://0.0.0.0:3000/api/posts";
+    let url = this.serverUrl + '/posts';
     return this.http.get(url, {headers: this.headers}).map(res => res.json()).catch(err => {
       return Observable.throw(err);
     })
   }
   // get single post from api server
   getPost(id:string): Observable<Post>{
-    let url = 'http://0.0.0.0:3000/api/posts/' + id;
+    let url =  this.serverUrl + '/posts/' + id;
     return this.http.get(url, {headers: this.headers}).map(res => res.json()).catch(err => {
+      return Observable.throw(err);
+    });
+  }
+  createPost(post: Post): Observable<any>{
+    let url = this.serverUrl + '/posts';
+    return this.http.post(url, {headers: this.headers}).map(res => res.json()).catch(err => {
       return Observable.throw(err);
     });
   }
--- blog/src/app/post-form/post-form.component.ts
+++ blog/src/app/post-form/post-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../blog/post';
+import { PostService } from '../blog/post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-form',
@@ -9,13 +11,31 @@ import { Post } from '../blog/post';
 export class PostFormComponent implements OnInit {
 
   post: Post = new Post();
-  constructor() { }
+
+  errorMessage = '';
+
+  constructor(private postService: PostService,
+              private router: Router
+             ) { }
 
   ngOnInit() {
   }
  // on form submit
   onSubmit(){
     console.log(this.post);
+
+    // post this data to service
+    this.postService.createPost(this.post).subscribe(res => {
+      //successful post
+      console.log(res.id);
+
+      //direct to view post
+      this.router.navigate(['/blog', res.id]);
+
+    }, err => {
+      console.log(err);
+      this.errorMessage = 'an error saving the post';
+    });
   }
 
 }
